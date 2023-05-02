8ce093928e8b0d9302d61f06e7fa6a7ce7c32635
Steve Steiner <ssteinerX@ssteinerX.com>
2023-05-02 16:04:18 -0400

    Change default to not monkeypatch.

--- django_model_repr/models.py
+++ django_model_repr/models.py
@@ -3,26 +3,28 @@
 from django.db import models
 from django.conf import settings
 
+
 class Model(models.Model):
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
-	def __repr__(self):
-		fields = self._meta.get_fields()
-		buf = "<%s"%(self.__class__.__name__)
-		buf += "\n"
+    def __repr__(self):
+        fields = self._meta.get_fields()
+        buf = "<%s" % (self.__class__.__name__)
+        buf += "\n"
 
-		for field in fields:
-			if not field.concrete:
-				continue
+        for field in fields:
+            if not field.concrete:
+                continue
 
-			buf += "\t%s: %s"%(field.name, getattr(self, field.name))
-			buf += "\n"
+            buf += "\t%s: %s" % (field.name, getattr(self, field.name))
+            buf += "\n"
 
-		buf += ">"
+        buf += ">"
 
-		return buf
+        return buf
 
 
-if getattr(settings, "MODEL_REPR_MONKEY_PATCHING", True):
-	setattr(models, "Model", Model)
+# Don't touch my Monkey unless I specifically tell you to; he bites.
+if getattr(settings, "MODEL_REPR_MONKEY_PATCHING", False):
+    setattr(models, "Model", Model)
